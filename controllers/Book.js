2a05a09c7c750d137c581b9fa377cbbd60096694
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-05 17:07:34 +0300

    ft:adding_deleteBook_controller:backend

--- controllers/Book.js
+++ controllers/Book.js
@@ -43,3 +43,28 @@ exports.postBook = async (req,res,next) => {
         })
     }
 }
+
+// /api/v1/books/:id
+// @desc DELETE book
+// @access PUBLIC
+exports.deleteBook = async (req,res,next) => {
+    try{
+        const id = req.params.id 
+        const book = await Book.findById(id)
+        if(book){
+            await book.remove()
+            return res.status(404).json({
+                success:true,
+                // count:books.length,
+                message:'Delete sucessfully'
+            })
+        }
+    } catch (err){
+        console.log(`Error:${err}`)
+        return res.status(500).json({
+            success:false,
+            // count:books.length,
+            error:'Internal Server Error'
+        })
+    }
+}
--- routes/Books.js
+++ routes/Books.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { getBooks , postBook} = require('../controllers/Book')
+const { getBooks , postBook , deleteBook} = require('../controllers/Book')
 const router = express.Router()
 
 router.route('/').get(getBooks).post(postBook)
+router.route('/:id').delete(deleteBook)
 
 module.exports = router
